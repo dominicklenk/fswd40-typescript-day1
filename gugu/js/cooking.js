6656835d751f5e5ab47b8abaa13352200e3b5d86
matthiasliszt <mliszt.program@gmail.com>
2018-05-17 14:13:25 +0200

    product var

--- gugu/js/cooking.js
+++
@@ -1,4 +0,0 @@
-function cookingButton() {
-    var content = "\n               <h1>Spaghetti Bolognese</h1>\n\n               <h3>Ingredients</h3>\n<p>2 tbsp olive oil</p>\n<p>400g/14oz beef mince</p>\n<p>1 onion, diced</p>\n<p>2 garlic cloves, chopped</p>\n<p>100g/3\u00BDoz carrot, grated</p>\n<p>2 x 400g tin chopped tomatoes</p>\n<p>400ml/14fl oz stock (made from stock cube. Ideally beef, but any will do)</p>\n<p>400g/14oz dried spaghetti</p>\n<p>salt and pepper</p>";
-    $('#myContent').html(content);
-}
---
+++ gugu/js/product.js
@@ -0,0 +1,10 @@
+var productSample=`<div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading" id='${productId}'>${productName}</div>
+        <div class="panel-body">
+          <img src='${productImage}' class="img-responsive" style="width:100%" alt="Image">
+        </div>
+        <div class="panel-footer">&#9734;&#9734;&#9734;&#9734;&#9734;</div>
+      </div>
+    </div>`;
+
