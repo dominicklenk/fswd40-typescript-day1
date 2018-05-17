2c0b5a96fd5db2d284a5095e5d45c0f4074cb540
dominicklenk <dominic_klenk@yahoo.at>
2018-05-17 14:25:28 +0200

    add file

---
+++ dominic/js/array.js
@@ -0,0 +1,61 @@
+var Products = [
+    {
+        name: "Bruschetta",
+        price: 4.70,
+        myPhoto: ""
+    },
+    { name: "Caprese salad",
+        price: 4,
+        myPhoto: ""
+    },
+    {
+        name: "Carpaccio",
+        price: 13,
+        myPhoto: ""
+    },
+    {
+        name: "Carbonara",
+        price: 10,
+        myPhoto: ""
+    },
+    {
+        name: "Calzone",
+        price: 11.50,
+        myPhoto: ""
+    },
+    {
+        name: "Margherita",
+        price: 11,
+        myPhoto: ""
+    },
+    {
+        name: "Gnocchi",
+        price: 6.90,
+        myPhoto: ""
+    },
+    {
+        name: "Clam Linguini",
+        price: 8.50,
+        myPhoto: ""
+    },
+    {
+        name: "Calamari Fritti",
+        price: 7.40,
+        myPhoto: ""
+    },
+    {
+        name: "Parmesan Cortoletta",
+        price: 8.80,
+        myPhoto: ""
+    },
+    {
+        name: "Carciofi alla Romana",
+        price: 6.20,
+        myPhoto: ""
+    },
+    {
+        name: "Gelato",
+        price: 3.50,
+        myPhoto: ""
+    }
+];
